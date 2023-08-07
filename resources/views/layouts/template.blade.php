0ab923caca66c6b05e4781a120acdfdf5cd6a0f2
kouhei0128 <kouhei1412@outlook.jp>
2023-08-08 02:33:08 +0900

    Add template.css file

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,31 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/template.css')  }}" >
     <title>@yield('title')</title>
 </head>
 <body>
-    <header>
-        <h1>@yield('title') 危険度:@yield('level')</h1>
-    </header>
+    <div class="container text-center">
+        <header>
+            <h1 class="fw-bold">@yield('title') 危険度:@yield('level')</h1>
+        </header>
     
-    <div class="container">
-        <p>被害想定</p>
+        <p class="fw-bold">被害想定</p>
         <ul>
             @yield('damage-list')
         </ul>
         <p>@yield('title')例: @yield('example')</p>
         
-        <h2>脆弱例</h2>
+        <h2 class="fw-bold">脆弱例</h2>
         @yield('bad-cont')
         
-        <h2>対策例</h2>
+        <h2 class="fw-bold">対策例</h2>
         @yield('good-cont')
 
         @yield('link')
-    </div>
+    
 
-    <footer>
-        <p>&copy; 2023 kawagisi</p>
-    </footer>
+        <footer>
+            <p>&copy; 2023 kawagisi</p>
+        </footer>
+    </div>
 </body>
 </html>
