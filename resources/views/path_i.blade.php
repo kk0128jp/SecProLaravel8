c63aaccf56a4c5f0ddfae7217b8f53361c0ca7a6
kouhei0128 <kouhei1412@outlook.jp>
2023-08-08 19:49:11 +0900

    Add path_i.css file

--- resources/views/path_i.blade.php
+++ resources/views/path_i.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.template')
 
+@section('head-link')
+    <link rel="stylesheet" href="{{ asset('/css/path_i.css') }}">
+@endsection
+
 @section('title')
     ディレクトリトラバーサル
 @endsection
@@ -9,7 +13,7 @@
 @endsection
 
 @section('damage-list')
-    <li>不正なファイル閲覧、改ざん、削除</li>
+    <li class="damage-list">不正なファイル閲覧、改ざん、削除</li>
 @endsection
         
 @section('example')
@@ -19,30 +23,30 @@
 @section('bad-cont')
     <form action="{{ route('post_path_i') }}" method="post">
         @csrf
-        <p>ファイルパスを入れてね</p>
+        <p class="cont-txt">ファイルパスを入れてね</p>
         <input type="text" name="path">
         <input type="hidden" name="measures" value="unmeasured">
-        <input type="submit" value="送信">
+        <button class="button" type="submit">入力</button>
     </form>
 @endsection
     
 @section('good-cont')
     <ul>
-        <li>外部からファイルパスを受け取らない</li>
-        <li>basename()の使用</li>
+        <li class="good-list">外部からファイルパスを受け取らない</li>
+        <li class="good-list">basename()の使用</li>
     </ul>
     <form action="{{ route('post_path_i') }}" method="post">
         @csrf
         <input type="text" name="path">
         <input type="hidden" name="measures" value="remedied">
-        <input type="submit" value="送信">
+        <button class="button" type="submit">入力</button>
     </form>
     @isset( $e_msg )
-        {{ $e_msg }}
+        <p class="e-msg">{{ $e_msg }}</p>
     @endisset
 
 @endsection
 
 @section('link')
-    <p><a href="{{ route('home') }}">ホームへ</a></p>
+    <p class="link"><a href="{{ route('home') }}">ホームへ</a></p>
 @endsection
