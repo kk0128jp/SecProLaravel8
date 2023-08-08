5ec1e1835cba36ec671400c0fdb458515b729134
kouhei0128 <kouhei1412@outlook.jp>
2023-08-08 21:01:52 +0900

    Fix xss_rem.blade.php design

--- resources/views/xss_rem.blade.php
+++ resources/views/xss_rem.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.xss_template')
 
+@section('head-link')
+    <link rel="stylesheet" href="{{ asset('/css/xss_unm.css') }}">
+@endsection
+
+@section('level')
+    <span style="color: yellow;">中</span>
+@endsection
+
 @section('title')
     クロスサイト・スクリプティング(XSS)
 @endsection
@@ -18,13 +26,13 @@
         @csrf
         <input type="text" name="text">
         <input type="hidden" name="mode" value="rem">
-        <input type="submit" value="投稿">
+        <button class="submit-button" type="submit">投稿</button>
     </form>
     <p>投稿文</p>
     @isset ( $obj )
         <ul>
             @foreach ($obj as $data)
-                <li>{{ $data->comment }}</li>
+                <li class="post-list">{{ $data->comment }}</li>
             @endforeach
         </ul>
     @endisset
