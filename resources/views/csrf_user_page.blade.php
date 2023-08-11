d6c36ea7cc30e4ac2b4f6b9a5651d52f736242cd
kouhei0128 <kouhei1412@outlook.jp>
2023-08-12 04:56:20 +0900

    Fix csrf_user_page.css file

--- resources/views/csrf_user_page.blade.php
+++ resources/views/csrf_user_page.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.csrf_template')
 
+@section('head-link')
+    <link rel="stylesheet" href="{{ asset('/css/csrf_user_page.css') }}">
+@endsection
+
 @section('cont')
     <h2>アカウント情報</h2>
-    <p>ユーザー名</p>
+    <p class="acc-info">ユーザー名</p>
     {{ $name }}
-    <p>メールアドレス</p>
+    <p class="acc-info">メールアドレス</p>
     {{ $email }}
     <br>
     <button class="submit-button"><a href="{{ route('get_csrf_edit') }}">編集</a></button>
@@ -13,5 +17,5 @@
         {{ $msg }}
     @endisset
     <br>
-    <a href="{{ route('get_csrf_login') }}">ログインページへ</a>
+    <a class="to-login-link" href="{{ route('get_csrf_login') }}">ログインページへ</a>
 @endsection
--- resources/views/layouts/csrf_template.blade.php
+++ resources/views/layouts/csrf_template.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
+    @yield('head-link')
+    <title>CSRF(クロスサイト・リクエスト・フォージェリ)</title>
 </head>
 <body> 
     <div class="container text-center">
-        <h1>CSRF(クロスサイト・リクエスト・フォージェリ)</h1>
+        <h1 class="title">CSRF(クロスサイト・リクエスト・フォージェリ)</h1>
         @yield('cont')
         <footer>&copy; 2023 kawagisi</footer>
     </div>
