ff78281da207e31a45471af3cd72a69d72f7e85f
kouhei0128 <kouhei1412@outlook.jp>
2023-08-13 03:03:08 +0900

    Fix csrf rem design

--- resources/views/csrf_rem_edit.blade.php
+++ resources/views/csrf_rem_edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.csrf_template')
 
+@section('head-link')
+    <link rel="stylesheet" href="{{ asset('/css/csrf_edit.css') }}">
+@endsection
+
 @section('cont')
     <h2>アカウント情報　編集</h2>
     <form action="{{ route('post_csrf_rem_edit') }}" method="post">
@@ -10,6 +14,7 @@
         <input type="text" name="new_email" value="{{ $email }}">
         <p>パスワード</p>
         <input type="password" name="password" required>
-        <input type="submit" value="編集">
+        <br>
+        <button class="submit-button" type="submit">編集</button>
     </form>
 @endsection
--- resources/views/csrf_rem_user_page.blade.php
+++ resources/views/csrf_rem_user_page.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.csrf_template')
 
+@section('head-link')
+    <link rel="stylesheet" href="{{ asset('/css/csrf_user_page.css') }}">
+@endsection
+
 @section('cont')
     <h2>アカウント情報</h2>
-    <p>ユーザー名</p>
+    <p class="acc-info">ユーザー名</p>
     {{ $name }}
-    <p>メールアドレス</p>
+    <p class="acc-info">メールアドレス</p>
     {{ $email }}
-    <button><a href="{{ route('get_csrf_rem_edit') }}">編集</a></button>
-    <button><a href="{{ route('get_csrf_rem_fake') }}">罠サイト</a></button>
+    <br>
+    <button class="submit-button"><a href="{{ route('get_csrf_rem_edit') }}">編集</a></button>
+    <button class="submit-button"><a href="{{ route('get_csrf_rem_fake') }}">罠サイト</a></button>
     @isset( $msg )
         {{ $msg }}
     @endisset
-    <a href="{{ route('get_csrf_login') }}">ログインページへ</a>
+    <br>
+    <a class="to-login-link" href="{{ route('get_csrf_login') }}">ログインページへ</a>
 @endsection
